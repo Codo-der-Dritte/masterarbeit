b73ff569dc29da988bca6b2eb93af33622f75c57
Codo_der_Dritte <h11706050@s.wu.ac.at>
2023-10-11 19:58:05 +0200

    First scripts. Just for self dicipline when coding later on

---
+++ scripts/main.R
@@ -0,0 +1,30 @@
+# Title: Stock Flow Consistent Model
+
+# Purpose : This script serves to run all function that are written
+#           and then stored in the folder R. These functions also 
+#           include function for data cleaning and processing.
+#           The script should use as little as possible of own code.
+#           Everything should be reproducible every time the R is 
+#           restarted. These main results regarding data and the model
+#           are then saved. Outputs are created in a different script
+#           which should be more flexiable and versatile.
+
+# Cite : citation() and used packages
+
+# DataFile:
+
+# Author: Alexander Toplitsch
+# Contact details: alexander.toplitsch@s.wu.ac.at
+
+# Date script created: We 11.10.2023 19:48 -------------
+# Date script last modified:  ----
+
+# Run all necessary functions
+
+
+
+
+# For good practice
+xfun::session_info()
+
+
